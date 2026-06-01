cfcd7157f61ab0e8c63d4e1d09bd8da9db9a0402
lmadevtools <lma.dev.tools@gmail.com>
2026-06-01 20:05:04 +0200

    Delete Utils directory

--- Utils/validator.py
+++
@@ -1,8 +0,0 @@
-import re
-
-#check if mail is valid
-def is_valid_email(email: str) -> bool:
-    if not isinstance(email, str):
-        return False
-    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
-    return re.match(pattern, email) is not None
