04a57224d9534ee0bbffe53d457e37c9d562acc1
lmadevtools <lma.dev.tools@gmail.com>
2026-06-10 08:52:04 +0200

    Add files via upload

---
+++ Utils/mail_sender.py
@@ -0,0 +1,35 @@
+#class not used. I just put it in case we would like to implement an automatic order for low stock products.
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from config import SMTP_SERVER, SMTP_PORT, MAIL_USER, MAIL_PASSWORD
+
+# Email configuration
+smtp_server = SMTP_SERVER
+smtp_port = SMTP_PORT
+sender_email = MAIL_USER
+sender_password = MAIL_PASSWORD
+recipient_email = 'supplier@example.com'
+subject = 'Restock necessary for low stock product'
+message = 'List of product where a restock is necessary :'
+
+# Create the email message
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = recipient_email
+msg['Subject'] = subject
+
+# Attach the message body
+msg.attach(MIMEText(message, 'plain'))
+
+try:
+    # Connect to the SMTP server
+    server = smtplib.SMTP(smtp_server, smtp_port)
+
+    # Send the email without authentication
+    server.sendmail(sender_email, recipient_email, msg.as_string())
+    print('Email sent successfully!')
+except Exception as e:
+    print('Email sending failed:', str(e))
+else:
+    server.quit()
